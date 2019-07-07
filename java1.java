3b14f3dcf840b26d0b9907ab9a5b226792a3ea63
aman884 <52627254+aman884@users.noreply.github.com>
2019-07-07 09:14:51 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,12 @@
+public class Main
+{
+public ststic void main(String[] args)
+{
+ /**************
+ 
+ write a program that adds A to B and store store the result in C
+ where A=3,B=4,C=5
+ ***************/
+ }//end of main
+
+}//end of class
