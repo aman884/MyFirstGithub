3cb8c1566825ad1cc3dcb53f575dc3bc554338f5
aman884 <52627254+aman884@users.noreply.github.com>
2019-07-07 09:53:02 -0400

    Update java1.java

--- java1.java
+++ java1.java
@@ -7,6 +7,9 @@ public ststic void main(String[] args)
  write a program that adds A to B and store store the result in C
  where A=3,B=4,C=5
  ***************/
+ int A=3:
+ int B=5:
+ int C=A+B;
  }//end of main
 
 }//end of class
